92fc894ce12ab5e1d55b6639f4cd2cc0381cde97
pablouix <pablouix@gmail.com>
2024-11-07 23:26:36 -0400

    Probando bd

--- CacaoDuarteAPI/CacaoDuarteAPI/Models/TiposCacao.cs
+++ CacaoDuarteAPI/CacaoDuarteAPI/Models/TiposCacao.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CacaoDuarteAPI.Models
 {
     public class TiposCacao
     {
         [Key]
+     //   [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string? Descripcion { get; set; }
