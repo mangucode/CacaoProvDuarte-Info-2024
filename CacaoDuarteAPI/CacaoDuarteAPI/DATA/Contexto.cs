3017d9ab0ae38545ef249403e871cdc9ed414dd8
pablouix <pablouix@gmail.com>
2024-11-07 22:07:41 -0400

    Creando  la carpeta DATA y el contexto

---
+++ CacaoDuarteAPI/CacaoDuarteAPI/DATA/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using CacaoDuarteAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CacaoDuarteAPI.DATA
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        DbSet<EmpresaCompraCacao>  EmpresaCompraCacaos { get; set; }    
+        DbSet<PrecioCacao> PreciosCacaos { get; set; }
+        DbSet<TiposCacao> TiposCacaos { get; set;}
+
+    }
+}
